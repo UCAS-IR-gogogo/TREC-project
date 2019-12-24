85515400e3b4bad09baf3c3dbea98c24de6cbcbf
dalek-who <839991169@qq.com>
2019-12-24 22:55:09 +0800

    黑色素瘤少了个瘤字，加上了

--- disease_dict.py
+++ disease_dict.py
@@ -8,7 +8,7 @@ with open(cwd/ "data/disease.txt") as fe, open(cwd / "data/疾病.txt") as fc:
     disease_ch = [line.strip() for line in fc.readlines()]
 
 disease_translate_dict = {en: ch for en, ch in zip(disease_en, disease_ch)}
-disease_translate_dict[None] = ""
+# disease_translate_dict[None] = ""
 dict_path = cwd / "data/disease_translate_dict.json"
 
 with open(dict_path, "w") as f:
