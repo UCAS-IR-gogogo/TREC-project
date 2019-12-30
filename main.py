1b9ce06de7a2731bcea14c07f2d08520ad69966e
dalek-who <839991169@qq.com>
2019-12-30 14:46:27 +0800

    demo

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from configs.config import config
 from preprocessing_doc import dataset_to_preprocessed_structure
 from preprocessing_topic import topics_to_preprocessed_structure
 from import_into_es import import_examples_into_es
+from es_search import es_search
+from filter_disease_gene_variant import filter_disease_gene_variant
 
 
 if __name__ == "__main__":
@@ -14,3 +16,6 @@ if __name__ == "__main__":
               2018: topics_to_preprocessed_structure(config.topic_path[2018])}
     # doc导入es构建索引
     import_examples_into_es(dataset_to_preprocessed_structure(config.doc_txt_dir))
+    # es查询
+    result_of_each_topic = es_search(topics[2018])
+    filtered_result_of_each_topic = filter_disease_gene_variant(result_of_each_topic)
